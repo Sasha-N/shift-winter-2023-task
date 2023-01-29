d9e04085798b8b3b7bde07269b79cd8c91560204
Alexandra <alexandra.nedomolkina@office.e2e4gu.ru>
2023-01-29 22:27:08 +0700

    add routing and game-page

--- rsp-game/src/app/app-routing.module.ts
+++ rsp-game/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import { GamePageComponent } from './game-page/game-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: LandingPageComponent },
+  { path: 'game', component: GamePageComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
