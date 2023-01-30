7809663e21fae0c39bda12122e92f29290e97b4e
Alexandra <alexandra.nedomolkina@office.e2e4gu.ru>
2023-01-31 01:55:14 +0700

    add result-page with routing

--- rsp-game/src/app/app-routing.module.ts
+++ rsp-game/src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LandingPageComponent } from './landing-page/landing-page.component';
 import { GamePageComponent } from './game-page/game-page.component';
+import { ResultPageComponent } from './result-page/result-page.component';
 
 const routes: Routes = [
   { path: '', component: LandingPageComponent },
-  { path: 'game', component: GamePageComponent }
+  { path: 'game', component: GamePageComponent },
+  { path: 'result', component: ResultPageComponent }
 ];
 
 @NgModule({
--- rsp-game/src/app/app.module.ts
+++ rsp-game/src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { LandingPageComponent } from './landing-page/landing-page.component';
 import { GamePageComponent } from './game-page/game-page.component';
 import { CardComponent } from './card/card.component';
+import { ResultPageComponent } from './result-page/result-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LandingPageComponent,
     GamePageComponent,
-    CardComponent
+    CardComponent,
+    ResultPageComponent
   ],
   imports: [
     BrowserModule,
