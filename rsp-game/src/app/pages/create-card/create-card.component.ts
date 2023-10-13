16e6c8040397c1d280e03443c7654f9973e49a4a
Alexandra <alexandra.nedomolkina@office.e2e4gu.ru>
2023-10-13 10:40:40 +0700

    add: paginator for pictures

--- rsp-game/src/app/pages/create-card/create-card.component.ts
+++ rsp-game/src/app/pages/create-card/create-card.component.ts
@@ -1,5 +1,4 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
-import { MatPaginator } from '@angular/material/paginator';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-create-card',
@@ -8,17 +7,37 @@ import { MatPaginator } from '@angular/material/paginator';
 })
 export class CreateCardComponent implements OnInit {
   public cardname: string = "";
+  public visibleCards: any[] = [];
+  public page: number = 0;
+  public size: number = 1;
+
+  public defaultCards = [
+    { id: 1, url: '../../../assets/picture-icons/add_photo_alternate_FILL0_wght400_GRAD0_opsz48.png' },
+    { id: 2, url: './../../assets/cards/paper.png' },
+    { id: 3, url: './../../assets/cards/scissor.png' },
+    { id: 4, url: './../../assets/cards/rock.png' }
+  ];
 
   constructor() { }
 
   ngOnInit(): void {
+    this.changePage({ pageIndex: this.page, pageSize: this.size });
   }
 
-  public saveName() {
+  //переписать
+  public saveName(): void {
     this.cardname = (<HTMLInputElement>document.getElementById("cardname")).value;
   }
 
-  @ViewChild('paginator') paginator: MatPaginator | undefined;
-  @ViewChild('paginatorPageSize') paginatorPageSize: MatPaginator | undefined;
+  public changePage(state: any): void {
+    let index = 0;
+    let startingIndex = state.pageIndex * state.pageSize;
+    let endingIndex = startingIndex + state.pageSize;
+
+    this.visibleCards = this.defaultCards.filter(() => {
+      index++;
+      return (index > startingIndex && index <= endingIndex) ? true : false;
+    });
+  }
 
 }
